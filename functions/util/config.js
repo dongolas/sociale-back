e5bb44a77f5af6375479b7f12ed8de2d4c45badf
dongolas <jodanous@gmail.com>
2020-03-06 15:55:42 +0200

    initial commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCIyL3i9sDSeDZ-YK_cLMGyNSKHruL7Abc",
+    authDomain: "sociale-89079.firebaseapp.com",
+    databaseURL: "https://sociale-89079.firebaseio.com",
+    projectId: "sociale-89079",
+    storageBucket: "sociale-89079.appspot.com",
+    messagingSenderId: "786130843072",
+    appId: "1:786130843072:web:01f518cad51fad3c8ac425",
+    measurementId: "G-8FS1NEP1PS"
+};
